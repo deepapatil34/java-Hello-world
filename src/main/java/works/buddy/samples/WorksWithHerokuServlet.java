d2ede31006604c5e19f6491a601c756ac8927b0d
deepapatil34 <81512753+deepapatil34@users.noreply.github.com>
2021-04-20 11:33:21 +0530

    Update WorksWithHerokuServlet.java

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("hi hello Buddy Works with Heroku");
+        writer.print("It works!! \nHello World\nBuddy Works with Heroku");
         writer.close();
     }
 }
